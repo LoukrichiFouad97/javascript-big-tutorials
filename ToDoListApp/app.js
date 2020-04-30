3ebe67569c198e9ae47347698cfa19f0cf527ce5
fouad97 <loukrichi21@gmail.com>
2020-05-01 00:39:49 +0100

    finishing The Project

--- ToDoListApp/app.js
+++ ToDoListApp/app.js
@@ -1,9 +1,10 @@
 const addItemBtn = document.getElementById("add-btn");
 const list = document.querySelector(".list");
 const userInput = document.getElementById("user-input");
-const tasks = document.getElementById("tasks");
-const completed = document.getElementById("completed");
+const tasks = document.getElementById("task");
+const completed = document.getElementById("task-completed");
 let task = 0;
+let taskCompleted = 0;
 
 class AddItems {
 	constructor(item) {
@@ -19,11 +20,21 @@ class AddItems {
 
 	addItemUI() {
 		const item = document.createElement("li");
-		item.textContent = this.item.value;
+		const itemTitle = document.createElement("h3");
+		itemTitle.setAttribute("id", "item-title");
+		itemTitle.textContent = this.item.value;
+		item.append(itemTitle);
 		list.prepend(item);
+
+		// delete item
 		this.addDelBtn(item);
 		const del = list.querySelector("#delete");
 		this.delItem(del, item);
+
+		// task completed
+		item.addEventListener("click", () => {
+			this.taskCompleted(item);
+		});
 	}
 
 	addDelBtn(parent) {
@@ -47,6 +58,16 @@ class AddItems {
 			task--;
 		}
 		tasks.textContent = task.toString();
+	}
+
+	taskCompleted(task) {
+		task.classList.toggle("active");
+		if (task.classList.contains("active")) {
+			taskCompleted++;
+		} else {
+			taskCompleted--;
+		}
+		completed.textContent = taskCompleted;
 	}
 }
 
